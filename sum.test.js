385ad1b455c8162450972f063311cedb97c96804
Rafida <rafidakn@gmail.com>
2023-02-12 18:11:56 +0100

    jest practice

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+const sum = require("./sum");
+
+describe("example tests", () => {
+  it("should add 1 + 2 to equal 3", () => {
+    const result = sum(1, 2);
+    expect(result).toBe(3);
+  });
+  it("object assignment", () => {});
+});
